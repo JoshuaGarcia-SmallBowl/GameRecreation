b9ee90b7133a4d35b0dacbaf456da649450cadb6
SmallBowl <721025@student.dusd.net>
2023-12-06 15:27:55 -0800

    Animations!!!!!!!!!!!!!!
    I animated him I animated him!!! UI started too.

--- GameRecreationGarciaP7/Assets/Scripts/Bird.cs
+++ GameRecreationGarciaP7/Assets/Scripts/Bird.cs
@@ -8,9 +8,11 @@ public class Bird : MonoBehaviour
     public float upForce = 200f;
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D> ();
     }
 
@@ -23,6 +25,7 @@ public class Bird : MonoBehaviour
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
             }
                                 
         }
@@ -30,5 +33,6 @@ public class Bird : MonoBehaviour
     void OnCollisionEnter2D()
     {
         isDead = true;
+        anim.SetTrigger("Die");
     }
 }
